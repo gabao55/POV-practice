d0551d85069373b9d89a45da1f7b278906475290
gabao55 <gabriel.s.rosin@gmail.com>
2022-12-19 16:40:02 -0300

    solves exercise

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace POV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int resultado1 = program.funcaoRetornaQuantitadePOV(0.1m, 90);
+            int resultado2 = program.funcaoRetornaQuantitadePOV(0.1m, 100);
+            int resultado3 = program.funcaoRetornaQuantitadePOV(0.2m, 70);
+
+            Console.WriteLine(resultado1.ToString());
+            Console.WriteLine(resultado2.ToString());
+            Console.WriteLine(resultado3.ToString());
+        }
+
+        int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado) {
+            return (int) Math.Floor(porcentagem * totalNegociado / (1 - porcentagem));
+        }
+    }
+}
